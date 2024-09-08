0bb56010b1b37a1e329a797ce014343b33854f43
tahir <tahir@endlessprojects.io>
2024-09-08 07:35:03 -0400

    Updated _app.tsx.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-// src/pages/_app.tsx
 import React from 'react';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Component {...pageProps} />
